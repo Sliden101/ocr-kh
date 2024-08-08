ab0538f77c06f249d4affc39b62bf3f45ad18ca8
Sliden101 <chessplayz11@gmail.com>
2024-08-08 20:32:49 +0700

    please

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,11 +7,15 @@ import cors from "cors";
 const app = express();
 
 app.disable("x-powered-by");
-app.use(cors);
 app.use(express.json({limit: '50mb'}));
 app.use(helmet());
 app.use("/", registerRoutes());
 app.use(errorHandler);
 
+const allowedOrigins = ['http://localhost:3000','https://ocr-kh.vercel.app/'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
 
 export default app;
