dda24af6d1eb2955fcc1371789df4da9d222e718
Sliden101 <chessplayz11@gmail.com>
2024-08-08 19:46:25 +0700

    ok this has to work

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,23 @@ import { errorHandler } from "~/middleware/errorHandler";
 import { registerRoutes } from "~/utils/routeRegister";
 import express from "express";
 import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
 
 app.disable("x-powered-by");
+app.use(cors);
 app.use(express.json({limit: '50mb'}));
 app.use(helmet());
 app.use("/", registerRoutes());
 app.use(errorHandler);
+
+const allowedOrigins = ['http://localhost:6969','https://ocr.sdgclub.site'];
+
+const options: cors.CorsOptions = {
+    origin: allowedOrigins
+};
+
+app.use(cors(options));
 
 export default app;
