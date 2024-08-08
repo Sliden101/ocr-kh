d366eadcb950164e6fbffddce8700c58c70f387c
Sliden101 <chessplayz11@gmail.com>
2024-08-08 21:39:38 +0700

    prab phg meas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,17 +8,27 @@ const app = express();
 
 app.disable("x-powered-by");
 
-const allowedOrigins = ["*"];
+// Specify the allowed origins
+const allowedOrigins = ["https://ocr-kh.vercel.app", "http://localhost:3000"];
 
-const options: cors.CorsOptions = {
-  origin: allowedOrigins
+const corsOptions: cors.CorsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin as string) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
-app.use(cors(options));
 
-app.use(express.json({limit: '50mb'}));
+app.use(cors(corsOptions));
+
+app.use(express.json({ limit: '50mb' }));
 app.use(helmet());
 app.use("/", registerRoutes());
 app.use(errorHandler);
-
 
 export default app;
